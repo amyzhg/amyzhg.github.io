20237d3d1d5c24599b61c2b286ec4a0b882f03af
Amy <zhangamy537@gmail.com>
2024-09-25 11:01:59 -0600

    Working on interactive

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -5,6 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let vaultboy;
+
+function preload(){
+  vaultboy = loadImage("vaultboy.gif");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +17,11 @@ function setup() {
 
 function draw() {
   background(0);
+  image(vaultboy, 200, 300);
 }
 
 
+
+// switch tabs for arrow keys 
+// use time clock
+// 
